9e3995909b78fefd961c73d5fc88c66e343aad0d
jamoliddinbe <bosuvay1@gmail.com>
2022-09-16 06:38:33 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,230 @@
+// FOR 
+
+// 1
+
+// var k = 1;
+// var n = 10;
+// for(var i = 1; i <= n; i++ ) {
+//     console.log(i)
+// }
+
+// 2
+
+// let a = 1
+// let b = 100
+// for (let i = 1; i <= b; i++ ){
+//     console.log(i);
+// }
+
+// 3
+
+// let a = 1
+// let b = 10
+// for ( let i = 2; i < b; i++ ){
+//     console.log(i);
+// }
+
+// 4
+
+// let kg = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+// let cost = 10000
+// for(let i = 1; i <= kg.length; i++ ){
+//     console.log( i * cost);
+// }
+
+// 5
+
+// let kg = [ 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1 ]
+// let cost = 10000
+// for(let i = 1; i <= kg.length; i++ ){
+//     console.log( i / 0.001);
+// }
+
+// 6
+
+// let kg = [ 1.2, 1.4, 1.6, 1.8, 2 ]
+// let cost = 10000
+// for(let i = 1; i <= kg.length; i++ ){
+//     console.log( i  / 0.00008 );
+// }
+
+// 7
+
+// let a = 1
+// let b = 10
+// for ( let i = 1; i <= b; i++  ){
+//     console.log(i+a);
+// } 
+
+
+
+// SWITCH 
+
+// 1
+
+// let numbers =  1
+// switch (numbers) {
+//     case 1:
+//         console.log("monday");
+//         break;
+//         case 2:
+//         console.log("tuesday");
+//         break;
+//         case 3:
+//         console.log("wednesday");
+//         break;
+//         case 4:
+//         console.log("thirsday");
+//         break;
+//         case 5:
+//         console.log("friday");
+//         break;  
+//         case 6:
+//         console.log("saturday");
+//         break;  
+//         case 7:
+//         console.log("sunday");
+//         break;
+
+//     default:
+//         console.log("Ooops, anything not found");
+//         break;
+// }
+ 
+// 2
+
+// let k = 5
+// switch (k) {
+//     case 1:
+//         console.log("yomon");
+//         break;
+//         case 2:
+//     console.log("qoniqarsiz");
+//     break;
+//     case 3:
+//          console.log("qoniqarli");
+//          break;  
+//          case 4:
+//         console.log("yaxshi");
+//         break; 
+//         case 5:
+//         console.log("alo");
+//         break;     
+//     default:
+//         console.log("sen vaabshe 0san");
+//         break;
+// }
+
+// 3
+
+// let month = 1
+// switch (month) {
+//     case 12:
+//         console.log("Winter");
+//         break;
+//         case 1:
+//             console.log("Winter");
+//             break;
+//             case 2:
+//                 console.log("Winter");
+//                 break;
+//                 case 3:
+//                     console.log("Spring");
+//                     break;
+//                     case 4:
+//                         console.log("Spring");
+//                         break;
+//                         case 5:
+//                             console.log("Spring");
+//                             break;
+//                             case 6:
+//                                 console.log("Summer");
+//                                 break;
+//                                 case 7:
+//                                     console.log("Summer");
+//                                     break;
+//                                     case 8:
+//                                         console.log("Summer");
+//                                         break;
+//                                         case 9:
+//                                             console.log("Autumn");
+//                                             break;
+//                                             case 10:
+//                                                 console.log("Autumn");
+//                                                 break;
+//                                                 case 11:
+//                                                     console.log("Autumn");
+//                                                     break;
+//     default:
+//         console.log("ERROR");
+//         break;
+// }
+
+// 4
+
+// let month = 2
+// switch (month) {
+//     case 1:
+//         console.log(31);
+//         break;
+//         case 2:
+//             console.log(28);
+//             break;
+//             case 3:
+//                 console.log(30);
+//                 break;
+//                 case 4:
+//                     console.log(31);
+//                     break;
+//                     case 5:
+//                         console.log(29);
+//                         break;
+//                         case 6:
+//                             console.log(30);
+//                             break;
+//                             case 7:
+//                                 console.log(31);
+//                                 break;
+//                                 case 8:
+//                                     console.log(30);
+//                                     break;
+//                                     case 9:
+//                                         console.log(31);
+//                                         break;
+//                                         case 10:
+//                                             console.log(30);
+//                                             break;
+//                                             case 11:
+//                                                 console.log(31);
+//                                                 break;
+//                                                 case 12:
+//                                                     console.log(30);
+//                                                     break;
+//     default:
+//         console.log("error");
+//         break;
+// }
+
+// 6
+
+// let mark = 5
+// switch (mark) {
+//     case 1:
+//         console.log(100000);
+//         break;
+//         case 2:
+//             console.log(200000);
+//             break;
+//             case 3:
+//                 console.log(300000);
+//                 break;
+//                 case 4:
+//                     console.log(400000);
+//                     break;
+//                     case 5:
+//                         console.log(500000);
+//                         break;
+//     default:
+//         console.log(0);
+//         break;
+// }
